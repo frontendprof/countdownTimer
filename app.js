62a5011a246cc229e3caa1280580fcfd0d995f4a
AbdulMalik <abdumaliksharipov@gmail.com>
2020-07-10 23:14:21 +0500

    Setting up

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+// Defining global variables
+
+const endDate=document.querySelector("input[name='endDate']");
+
+const clock = document.querySelector(".clock");
+
+
+// Adding event listener
+
+endDate.addEventListener("change",e=>{
+    e.preventDefault();
+})
+
